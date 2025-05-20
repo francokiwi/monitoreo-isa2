66a7beb8b4b64c38c8b3264047c81813ea9966f6
Franco <franco@basados.org>
2025-05-20 10:43:18 -0300

    readme

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using OpenTelemetry.Extensions.Hosting;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Exporter;
@@ -8,8 +7,12 @@ using System.Diagnostics.Metrics;
 var builder = WebApplication.CreateBuilder(args);
 
 
+var metrics = new CustomMetrics();
+
 builder.Services.AddOpenTelemetry()
-        .WithMetrics(metricsBuilder => { metricsBuilder
+        .WithMetrics(metricsBuilder =>
+        {
+            metricsBuilder
         .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Dice-App"))
         .AddAspNetCoreInstrumentation()
         .AddMeter("CustomMetrics")
@@ -18,7 +21,7 @@ builder.Services.AddOpenTelemetry()
                 options.Endpoint = new Uri("http://localhost:4317");
                 options.Protocol = OtlpExportProtocol.Grpc;
             })
-		.AddView(instrumentName: "dice_results", new ExplicitBucketHistogramConfiguration {Boundaries = new double[] { 1, 2, 3, 4, 5, 6 }});
+        .AddView(instrumentName: "dice_results", new ExplicitBucketHistogramConfiguration { Boundaries = new double[] { 1, 2, 3, 4, 5, 6 } });
         });
 
 var app = builder.Build();
@@ -26,10 +29,9 @@ var app = builder.Build();
 
 string HandleRollDice(string? player)
 {
-	var metrics = new CustomMetrics();
     var result = Random.Shared.Next(1, 7);
-	metrics.DiceRolls();
-	metrics.DiceResults(result);
+    metrics.DiceRolls();
+    metrics.DiceResults(result);
 
     return result.ToString();
 }
@@ -40,33 +42,33 @@ app.Run();
 
 class CustomMetrics
 {
-	private readonly Meter _meter;
-	private readonly Counter<long> _diceRollsCounter;
-	private readonly Histogram<int> _diceResultsHistogram;
+    private readonly Meter _meter;
+    private readonly Counter<long> _diceRollsCounter;
+    private readonly Histogram<int> _diceResultsHistogram;
 
-	public CustomMetrics()
-	{
-		_meter = new Meter("CustomMetrics");
-		_diceRollsCounter = _meter.CreateCounter<long>(
-			"dice_rolls",
-			unit: "1",
-			description: "Counts the number of dice rolls"
-		);
+    public CustomMetrics()
+    {
+        _meter = new Meter("CustomMetrics");
+        _diceRollsCounter = _meter.CreateCounter<long>(
+            "dice_rolls",
+            unit: "1",
+            description: "Counts the number of dice rolls"
+        );
 
-		_diceResultsHistogram = _meter.CreateHistogram<int>(
-			"dice_results",
-			unit: "1",
-			description: "Records the results of dice rolls"
-		);
-	}
+        _diceResultsHistogram = _meter.CreateHistogram<int>(
+            "dice_results",
+            unit: "1",
+            description: "Records the results of dice rolls"
+        );
+    }
 
-	public void DiceRolls(long value = 1)
-	{
-		_diceRollsCounter.Add(value);
-	}
+    public void DiceRolls(long value = 1)
+    {
+        _diceRollsCounter.Add(value);
+    }
 
-	public void DiceResults(int result)
-	{
-		_diceResultsHistogram.Record(result);
-	}
+    public void DiceResults(int result)
+    {
+        _diceResultsHistogram.Record(result);
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # monitoreo-isa2
+
+## Instrucciones para ejecutar el proyecto
+
+1. Correr contenedores de docker
+```sh
+docker compose up -d
+```
+
+2. Ejecutar el proyecto de dotnet
+```sh
+dotnet run
+```
+
+## Explicacion de la solucion
+
+La solucion consiste de una api web que expone la URI /rolldice, que devuelve un numero del 1 al 6 cuando recibe un GET.
+Esta utiliza OpenTelemetryProtocol como exportador para enviar estadisticas de la app a Prometheus.
+
+Las estadisticas que envia son:
+- Contador de cantidad de tiradas
+- Histograma de valores de las tiradas
